6a89020dfacf103ebc332acbb08072f51d65f29b
Luke Scott <luke@webconnex.com>
2014-10-19 14:42:12 -0700

    Update tld list, use sorted log search

--- tld.go
+++ tld.go
@@ -14,22 +14,13 @@
 
 package validate
 
-func IsValidTLD(str string) bool {
-	length := len(str)
-	match := 0
-	for _, tld := range tldList {
-		if len(tld) != length {
-			continue
-		}
-		match = 0
-		for i := 0; i < length; i++ {
-			if tld[i] == str[i] || tld[i]+32 == str[i] {
-				match++
-			}
-		}
-		if match == length {
-			break
-		}
-	}
-	return match == length
+import (
+	"sort"
+	"strings"
+)
+
+func IsValidTLD(tld string) bool {
+	tldUpper := strings.ToUpper(tld)
+	index := sort.SearchStrings(tldList, tldUpper)
+	return index < len(tldList) && tldList[index] == tldUpper
 }
--- tld_scrape.go
+++ tld_scrape.go
@@ -24,6 +24,7 @@ import (
 	"net/http"
 	"path"
 	"runtime"
+	"sort"
 	"strings"
 )
 
@@ -35,39 +36,41 @@ func main() {
 	}
 
 	// Scrape TLD list
-	r, err := http.Get("http://data.iana.org/TLD/tlds-alpha-by-domain.txt")
+	url := "http://data.iana.org/TLD/tlds-alpha-by-domain.txt"
+	r, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(r.Body)
-	common := []string{"COM", "NET", "ORG"}
-	list := []string{}
-	list = append(list, common...)
-Next:
+	comments := []string{}
+	tldList := []string{}
 	for scanner.Scan() {
-		tld := string(scanner.Text())
-		if tld[0] == '#' {
+		line := string(scanner.Text())
+		if line[0] == '#' {
+			comments = append(comments, strings.TrimLeft(line, "# "))
 			continue
 		}
-		if tld[:2] == "XN" {
+		if len(line) > 4 && line[:4] == "XN--" {
 			continue
 		}
-		for _, ctld := range common {
-			if tld == ctld {
-				continue Next
-			}
-		}
-		list = append(list, tld)
+		tldList = append(tldList, line)
 	}
+	sort.Sort(sort.StringSlice(tldList))
 
 	// Write tld_list.go
 	buf := new(bytes.Buffer)
 	buf.WriteString("package validate\n\n")
-	buf.WriteString("var tldList = []string")
-	buf.WriteByte('{')
-	buf.WriteByte('"')
-	buf.WriteString(strings.Join(list, `","`))
-	buf.WriteByte('"')
-	buf.WriteByte('}')
+	buf.WriteString("// ")
+	buf.WriteString(url)
+	buf.WriteByte('\n')
+	if len(comments) > 0 {
+		buf.WriteString("// ")
+		buf.WriteString(comments[0])
+		buf.WriteByte('\n')
+	}
+	buf.WriteByte('\n')
+	buf.WriteString("var tldList = []string{\n\t\"")
+	buf.WriteString(strings.Join(tldList, "\",\n\t\""))
+	buf.WriteString("\",\n}")
 	ioutil.WriteFile(path.Dir(self)+"/tld_list.go", buf.Bytes(), 0644)
 }
